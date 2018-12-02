01919fa34267eb8e0434d6144f0308e5ccf82f4f
Erik IT <erik.kaju@gmail.com>
2018-12-02 02:47:29 +0200

    Robot control logic before pre-competition night - many improvements needed!

--- robots/neve/stateholder.cpp
+++ robots/neve/stateholder.cpp
@@ -14,14 +14,12 @@ char* StateHolder::toString() const
         case Neve::WAITING_START: return "WAITING_START"; //1
         case Neve::FIND_BALL: return "FIND_BALL"; //2
         case Neve::IMPROVE_CONTACT_WITH_BALL: return "IMPROVE_CONTACT_WITH_BALL"; //3
-        case Neve::FIND_GOAL: return "FIND_GOAL"; //4
-        case Neve::GOAL_RANDOM: return "GOAL_RANDOM";
-        case Neve::BALL_RANDOM: return "BALL_RANDOM";
-        case Neve::NEW_KICK_POSITION: return "NEW_KICK_POSITION";
         case Neve::ESCAPE_BLACK_LINE: return "ESCAPE_BLACK_LINE";
         case Neve::START_ESCAPE_BLACK_LINE: return "START_ESCAPE_BLACK_LINE";
         case Neve::DISTRONIC_TURN: return "DISTRONIC_TURN";
         case Neve::ESCAPE_GOAL_TOO_CLOSE: return "GOAL_TOO_CLOSE";
+        case Neve::THROW: return "THROW";
+
     }
 }
 
